a1f87dc6b82a16780df21495bfe62a13dea680be
Tidjani Belmansour <tidjani.belmansour@admtl.com>
2024-02-16 06:23:09 -0500

    commit of the initial version of the app

---
+++ CalicotGaming/CalicotGaming.Web/Models/CalicotGamingDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CalicotGaming.Web.Models
+{
+    public class CalicotGamingDbContext : DbContext
+    {
+        public CalicotGamingDbContext(DbContextOptions<CalicotGamingDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
+
+
+// dotnet ef dbcontext scaffold "Server=localhost;Database=CalicotGaming;User Id=sa;Password=Password123;" Microsoft.EntityFrameworkCore.SqlServer -o Models
---
+++ CalicotGaming/CalicotGaming.Web/Models/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace CalicotGaming.Web.Models
+{
+    
+    public class Product
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public decimal Price { get; set; }
+        public string? PhotoUrl { get; set; }
+        public string? Description { get; set; }        
+    }
+}
