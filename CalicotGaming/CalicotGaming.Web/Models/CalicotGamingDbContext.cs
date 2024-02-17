a0ad1ebc98b6e0baf4d6340f1481ebe0fda98da0
Tidjani Belmansour <tidjani.belmansour@admtl.com>
2024-02-16 19:41:26 -0500

    fixed warnings

--- CalicotGaming/CalicotGaming.Web/Models/CalicotGamingDbContext.cs
+++ CalicotGaming/CalicotGaming.Web/Models/CalicotGamingDbContext.cs
@@ -8,7 +8,7 @@ namespace CalicotGaming.Web.Models
             : base(options)
         {
         }
-        public DbSet<Product> Products { get; set; }
+        public DbSet<Product>? Products { get; set; }
     }
 }
 
